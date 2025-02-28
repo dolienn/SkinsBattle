9aebcd413baad62234c9f47c3905a758b9e666e0
dolienn <thedolien@gmail.com>
2025-02-28 19:47:13 +0100

    Initial commit

---
+++ src/main/java/pl/dolien/skinsbattle/SkinsBattleApplication.java
@@ -0,0 +1,13 @@
+package pl.dolien.skinsbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinsBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinsBattleApplication.class, args);
+	}
+
+}
