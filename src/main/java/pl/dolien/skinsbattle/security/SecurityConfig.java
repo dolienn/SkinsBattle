d1397b77635fcfcd0999aec6c6bedb917404169c
dolienn <thedolien@gmail.com>
2025-02-28 22:37:25 +0100

    Added application-dev.yml, application.yml and implemented docker, postgres, keycloak

---
+++ src/main/java/pl/dolien/skinsbattle/security/SecurityConfig.java
@@ -0,0 +1,79 @@
+package pl.dolien.skinsbattle.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSocket
+public class SecurityConfig {
+
+    @Value("${angular.url}")
+    private String angularUrl;
+
+    private static final String[] PERMITTED_URLS = {
+            "/auth/**",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html",
+            "/ws/**"
+    };
+
+    private static final List<String> ALLOWED_HEADERS =
+            Arrays.asList(ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION);
+
+    private static final List<String> ALLOWED_METHODS =
+            Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(PERMITTED_URLS)
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(auth ->
+                        auth.jwt(token ->
+                                token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList(angularUrl));
+        config.setAllowedHeaders(ALLOWED_HEADERS);
+        config.setAllowedMethods(ALLOWED_METHODS);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
